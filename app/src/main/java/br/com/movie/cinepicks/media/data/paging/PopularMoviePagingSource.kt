c69c4ce9fcecbe2cff2fe4d3ea87ca1ce6fb9853
Maia <gbmmaia.contato@gmail.com>
2023-07-11 20:21:29 -0300

    feat (network): Created PopularMoviePagingSource to paginate movie Data

---
+++ app/src/main/java/br/com/movie/cinepicks/media/data/paging/PopularMoviePagingSource.kt
@@ -0,0 +1,31 @@
+package br.com.movie.cinepicks.media.data.paging
+
+import android.util.Log
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import br.com.movie.cinepicks.network.TMDBApi
+import br.com.movie.cinepicks.network.dtos.MediaDto
+import javax.inject.Inject
+
+class PopularMoviePagingSource @Inject constructor(
+    private val movieService: TMDBApi
+) : PagingSource<Int, MediaDto>() {
+
+    override fun getRefreshKey(state: PagingState<Int, MediaDto>): Int? = state.anchorPosition
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MediaDto> {
+        val currentPage = params.key ?: 1
+        return try {
+            val response = movieService.getPopularMovies(page = currentPage)
+            LoadResult.Page(
+                data = response.medias,
+                prevKey = if (currentPage == 1) null else currentPage.minus(1),
+                nextKey = if (response.medias.isEmpty()) null else currentPage.plus(1)
+            )
+        } catch (e: Exception) {
+            Log.e("PagingSource", "PopularMovie: ${e.stackTrace}")
+            LoadResult.Error(e)
+        }
+    }
+
+}
